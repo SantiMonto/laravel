9e9676d45659d91cdaad5a45b15b503daf886041
Santiago Montoya <smonto67@gmail.com>
2024-03-05 19:17:16 -0500

    rutas en una sola linea de codigo

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomeController::class)->name('home');
 
-Route::controller(CursoController::class)->group(function () {
-    Route::get('cursos', 'index')->name('cursos.index');
-    Route::get('cursos/create', 'create')->name('cursos.create');
-    Route::post('cursos', 'store')->name('cursos.store');
-    Route::get('cursos/{curso}/edit', 'edit')->name('cursos.edit');
-    Route::get('cursos/{curso}/{categoría?}', 'show')->name('cursos.show');
-    Route::put('cursos/{curso}', 'update')->name('cursos.update');
-    Route::delete('cursos/{curso}', 'destroy')->name('cursos.destroy');
-});
+// Route::controller(CursoController::class)->group(function () {
+//     Route::get('cursos', 'index')->name('cursos.index');
+//     Route::get('cursos/create', 'create')->name('cursos.create');
+//     Route::post('cursos', 'store')->name('cursos.store');
+//     Route::get('cursos/{curso}/edit', 'edit')->name('cursos.edit');
+//     Route::get('cursos/{curso}/{categoría?}', 'show')->name('cursos.show');
+//     Route::put('cursos/{curso}', 'update')->name('cursos.update');
+//     Route::delete('cursos/{curso}', 'destroy')->name('cursos.destroy');
+// });
+
+Route::resource('cursos', CursoController::class);
